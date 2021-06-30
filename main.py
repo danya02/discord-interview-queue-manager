68eea6650d6162e9a1b6166c1a8a0116d4104e96
Danya Generalov <danya@danya02.ru>
2021-06-30 10:16:15 +0100

    Adding framework for adding tracking of members not signed up for interview.

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import os
 import discord
+import asyncio
+from apscheduler.schedulers.async_ import AsyncScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.workers.async_ import AsyncWorker
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-CHAN = int(os.getenv('TARGET_CHANNEL'))
-ROLE = int(os.getenv('TARGET_ROLE'))
+LISTEN_CHAN = int(os.getenv('LISTEN_CHANNEL'))
+WAIT_ROLE = int(os.getenv('WAITING_ROLE'))
 
 client = discord.Client()
 
+
 @client.event
 async def on_message(message):
-    if message.channel.id == CHAN:
-      await message.author.add_roles(discord.Object(ROLE))
+    if message.channel.id == LISTEN_CHAN:
+      await message.author.add_roles(discord.Object(WAIT_ROLE))
+
+async def say_hello():
+    print('Hello World!')
+
+async def scheduler():
+    async with AsyncScheduler() as scheduler, AsyncWorker(scheduler.data_store):
+        await scheduler.add_schedule(say_hello, IntervalTrigger(seconds=60))
+        await scheduler.wait_until_stopped()
+
+def main():
+    loop = asyncio.get_event_loop()
+    discord_task = asyncio.create_task(client.start(TOKEN))
+    scheduler_task = asyncio.create_task(scheduler())
+
+    gathered = await asyncio.gather(discord_task, scheduler_task)
 
 client.run(TOKEN)
